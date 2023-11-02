24ddd96f97a2c14f3bd7b71d3ce1148510c2ce1b
Marco Ferreira <marcoantoniobferreira@gmail.com>
2023-11-02 19:24:04 -0300

    adding exception handling and a README

---
+++ README.md
@@ -0,0 +1,14 @@
+## Alura fórum
+
+This app is part of an Alura training track
+
+## Stack
+ - Kotlin 1.6.21
+ - Java 11
+ - Spring Boot 2.7.15
+ - Maven 4.0.0
+
+## How to run
+
+You could hit the `play` button if you are on InteliJ or if you want
+to run in standalone mode you could just type `mvn spring-boot:run`
--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,11 +5,11 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm (
-    @field:NotEmpty
-    @field:Size(min = 5, max = 100)
+    @field:NotEmpty(message = "Título não pode ser vazio")
+    @field:Size(min = 5, max = 100, message = "Título deve ter entre 5 e 100 caracteres")
     val titulo: String,
 
-    @field:NotEmpty
+    @field:NotEmpty(message = "Mensagem não pode ser vazia")
     val mensagem: String,
 
     @field:NotNull
